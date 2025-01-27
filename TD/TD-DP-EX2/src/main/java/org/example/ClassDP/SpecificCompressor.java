ff36790ce0f9ecf354e40f8e693aace408920775
MitarReda <154978104+MitarReda@users.noreply.github.com>
2025-01-27 21:50:26 +0100

    Delete TD/TD-DP-EX2 directory

--- TD/TD-DP-EX2/src/main/java/org/example/ClassDP/SpecificCompressor.java
+++
@@ -1,9 +0,0 @@
-package org.example.ClassDP;
-
-public class SpecificCompressor extends CompressorBase{
-    @Override
-    protected int[] compressDetails(int[] data) {
-        System.out.println("Applying specific compression algorithm...");
-        return data; // Exemple simple
-    }
-}
